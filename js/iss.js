995f651ed62dac8bc2e301d945ba61c04a4ea0be
David_Stichter <david.stichter3414@gmail.com>
2015-12-05 13:10:37 -0500

    New Functions, Shell layout

--- js/iss.js
+++ js/iss.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
   $(".btn-default").on("click", function(e){
       e.preventDefault();
-
+      var userAddress = $("#userAddress").val();
+      var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+      googleApiUrl += "key=AIzaSyBMRKa7-ZiGt-5oyuW8en1ln_zIDvGRUj0";
+      googleApiUrl += "&address=" + userAddress;
+      
       $.ajax({
         type: "GET",
         url: googleApiUrl,
@@ -11,16 +15,27 @@ $(document).ready(function(){
     });
 
   function googleApiSuccessHandler(response) {
-    var userAddress = $("#userAddress").val();
-    var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
-    googleApiUrl += "key=AIzaSyBMRKa7-ZiGt-5oyuW8en1ln_zIDvGRUj0";
-    googleApiUrl += "&address=" + userAddress;
+
 
     var geoLocation = response.results[0].geometry.location
 
-    var issPassover = {
+    var issPassoverParams = {
     "latitude": geoLocation.lat,
     "longitude": geoLocation.lng, 
     }
+
+    $.ajax({
+      type: "GET",
+      url: "http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON" + $.param(flickrApiParams),
+      success: issPassoverSuccessHandler
+    });
+
+
+
+  };
+
+  function issPassoverSuccessHandler(results){
+    console.log(results)
+  }
 });
 
