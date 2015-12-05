fdad3bc42209d84297735497840e4e5589c6ab17
David_Stichter <david.stichter3414@gmail.com>
2015-12-05 12:29:34 -0500

    Shellfor Home.html, event listener for button

--- js/iss.js
+++ js/iss.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+  $(".btn-default").on("click", function(e){
+      e.preventDefault();
 
+      $.ajax({
+        type: "GET",
+        url: googleApiUrl,
+        success: googleApiSuccessHandler
+      });
+
+    });
 });
