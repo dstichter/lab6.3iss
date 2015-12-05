ccab074cc7032f948c6468d847f098575a0a9aec
David_Stichter <david.stichter3414@gmail.com>
2015-12-05 12:37:32 -0500

    Google Api lat long, begining iss, ajax

--- js/iss.js
+++ js/iss.js
@@ -9,4 +9,18 @@ $(document).ready(function(){
       });
 
     });
+
+  function googleApiSuccessHandler(response) {
+    var userAddress = $("#userAddress").val();
+    var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+    googleApiUrl += "key=AIzaSyBMRKa7-ZiGt-5oyuW8en1ln_zIDvGRUj0";
+    googleApiUrl += "&address=" + userAddress;
+
+    var geoLocation = response.results[0].geometry.location
+
+    var issPassover = {
+    "latitude": geoLocation.lat,
+    "longitude": geoLocation.lng, 
+    }
 });
+
